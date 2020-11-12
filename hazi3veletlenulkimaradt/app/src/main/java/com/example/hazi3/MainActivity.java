75e3387bc891da7ba8b5f77d5f995b0264810920
Puskas Gabor <puskas.gabika@gmail.com>
2020-11-12 10:47:28 +0200

    hazik

---
+++ hazi3veletlenulkimaradt/app/src/main/java/com/example/hazi3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hazi3;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.hazi3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main3);
+    }
+}
